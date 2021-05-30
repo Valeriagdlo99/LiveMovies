1b48240a5bcf91e15f0a2cc09f53972ecdd830ba
valeriagdlo <A01705318@itesm.com>
2021-05-29 23:51:59 -0400

    Initial Commit

---
+++ LiveMovies/LiveMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveMoviesApp.swift
+//  LiveMovies
+//
+//  Created by user186890 on 5/29/21.
+//
+
+import SwiftUI
+
+@main
+struct LiveMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
